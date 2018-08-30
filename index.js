f32e126fd13026b1feb18f2608e031bf0ce9ac1e
Josh <grafton.josh@gmail.com>
2018-08-30 14:42:10 -0600

    completed periodic table

--- index.js
+++ index.js
@@ -25,30 +25,70 @@ document.addEventListener('DOMContentLoaded', function() {
       symbol: 'Ne'
     }
   ]
+  //Start my functions and variables
+
+  //Find table body
+  let tableBody = document.getElementsByTagName('tbody')[0]
+
+  //Create a row element
+  function createRow() {
+    let row = document.createElement('tr')
+    return row
+  }
+
+  //Create table data elements
+  function createTD() {
+    let tableData = document.createElement('td')
+    return tableData
+  }
+
+  //Create table head elements
+  function createTH() {
+    let tableHead = document.createElement('th')
+    return tableHead
+  }
+
+  //Add Rows for each element under table body
+  for (var i = 0; i < 5; i++) {
+    tableBody.appendChild(createRow(i))
+    let tableRow = document.getElementsByTagName('tr')[i + 1]
+    tableRow.appendChild(createTH(i)).innerText = elements[i]['number']
+    tableRow.appendChild(createTD(i)).innerText = elements[i]['symbol']
+    tableRow.appendChild(createTD(i)).innerText = elements[i]['name']
+  }
 })
 
-//Add Rows for each element
 //In each element, create a table head with the number
 //create a table data with symbol
 //create a table data with the name
 
 //Find table body
-let tableBody = document.getElementsByTagName('tbody')[0]
-
-//Create a row element
-function createRow() {
-  let row = document.createElement('tr')
-  return row
-}
-
-//Create a table head
-let tableHead = document.createElement('th')
-
-//create a table data
-let tableData = document.createElement('td')
-
-//Add the row in the HTML under the table body
-
-for (var i = 0; i < 5; i++) {
-  tableBody.appendChild(createRow(i))
-}
+// let tableBody = document.getElementsByTagName('tbody')[0]
+//
+// //Create a row element
+// function createRow() {
+//   let row = document.createElement('tr')
+//   return row
+// }
+//
+// //Create table data elements
+// function createTD() {
+//   let tableData = document.createElement('td')
+//   return tableData
+// }
+//
+// //Create table head elements
+// function createTH() {
+//   let tableHead = document.createElement('th')
+//   return tableHead
+// }
+//
+// //Add Rows for each element under table body
+// for (var i = 0; i < 5; i++) {
+//   tableBody.appendChild(createRow(i))
+//   let tableRow = document.getElementsByTagName('tr')[i + 1]
+//   tableRow.appendChild(createTH(i)).innerText = "hope this works"
+//   tableRow.appendChild(createTD(i)).innerText = 'Table data'
+//   tableRow.appendChild(createTD(i)).innerText = 'Table data2'
+//
+// }
